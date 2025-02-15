6c6397d036aae7f3bfaa4e96c6dd7af2b26bed0f
nkinard <nkinard7503@gmail.com>
2025-02-14 19:08:09 -0500

    recovery issue

---
+++ csce242/projects/part4/about-script.js
@@ -0,0 +1,4 @@
+//hamburger toggle switch
+document.getElementById("hamburger-toggle").onclick = () => {
+    document.getElementById("nav-list").classList.toggle("hide-small");
+  };
---
+++ csce242/projects/part4/spotlight-script.js
@@ -0,0 +1,48 @@
+//script by Nicholas Kinard
+//a lot of code help came from W3 Schools and
+
+//hamburger toggle switch
+document.getElementById("hamburger-toggle").onclick = () => {
+  document.getElementById("nav-list").classList.toggle("hide-small");
+};
+
+//pop ups for the spotlight
+document.getElementById("spotlightC").onclick = () => {
+  const spot = document.getElementById("modal-spotlightC");
+  spot.classList.remove("hide");
+
+  const close = spot.querySelector(".x-button");
+  close.onclick = () => {
+    spot.classList.add("hide");
+  };
+};
+
+document.getElementById("spotlightS").onclick = () => {
+  const spot = document.getElementById("modal-spotlightS");
+  spot.classList.remove("hide");
+
+  const close = spot.querySelector(".x-button");
+  close.onclick = () => {
+    spot.classList.add("hide");
+  };
+};
+
+document.getElementById("spotlightW").onclick = () => {
+  const spot = document.getElementById("modal-spotlightW");
+  spot.classList.remove("hide");
+
+  const close = spot.querySelector(".x-button");
+  close.onclick = () => {
+    spot.classList.add("hide");
+  };
+};
+
+document.getElementById("spotlightM").onclick = () => {
+  const spot = document.getElementById("modal-spotlightM");
+  spot.classList.remove("hide");
+
+  const close = spot.querySelector(".x-button");
+  close.onclick = () => {
+    spot.classList.add("hide");
+  };
+};
