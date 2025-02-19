7e73cd61f992076037e0ef0a1a2372f681d95880
nkinard <nkinard7503@gmail.com>
2025-02-19 12:20:59 -0500

    assignment update

--- csce242/assignments/assignment07/script.js
+++ csce242/assignments/assignment07/script.js
@@ -1,12 +1,21 @@
 document.getElementById("btn-stairs").onclick = () => {
     const stairs = document.getElementById("stair-js");
+    const climb = document.getElementById("btn-climb");
+    climb.classList.toggle("hide-section");
     stairs.classList.toggle("hide-section");
-    stairs.innerHTML = "";
 
-    for(let i=1; i <= 10; i++) {
-        const hr = document.createElement("hr");
-        hr.innerHMTL = i;
-        console.log(i);
-        stairs.append(hr);
+    if(stairs.classList.contains("hide-section")) {
+        stairs.innerHTML = "";
+    } else {
+        for(let i=1; i <= 10; i++) {
+            const hr = document.createElement("hr");
+            hr.innerHMTL = i;
+            console.log(i);
+            stairs.append(hr);
+        }
     }
-}
+};
+
+document.getElementById("btn-climb").onclick = () => {
+    console.log("hi");
+};
