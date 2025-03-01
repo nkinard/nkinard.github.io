c19287f4cc48a048c34040322f327140361d5d3e
nkinard <nkinard7503@gmail.com>
2025-03-01 00:37:20 -0500

    update

---
+++ csce242/projects/part5/library-script.js
@@ -0,0 +1,83 @@
+//script by Nicholas Kinard
+//a lot of code help came from W3 Schools and
+
+//hamburger toggle switch
+document.getElementById("hamburger-toggle").onclick = () => {
+  document.getElementById("nav-list").classList.toggle("hide-small");
+};
+
+//pop ups for the fish
+document.getElementById("redfish").onclick = () => {
+  const fish = document.getElementById("modal-fishR");
+  fish.classList.remove("hide");
+
+  const close = fish.querySelector(".x-button");
+  close.onclick = () => {
+    fish.classList.add("hide");
+  };
+};
+document.getElementById("trout").onclick = () => {
+  const fish = document.getElementById("modal-fishT");
+  fish.classList.remove("hide");
+
+  const close = fish.querySelector(".x-button");
+  close.onclick = () => {
+    fish.classList.add("hide");
+  };
+};
+document.getElementById("sheep").onclick = () => {
+  const fish = document.getElementById("modal-fishSH");
+  fish.classList.remove("hide");
+
+  const close = fish.querySelector(".x-button");
+  close.onclick = () => {
+    fish.classList.add("hide");
+  };
+};
+document.getElementById("bass").onclick = () => {
+  const fish = document.getElementById("modal-fishB");
+  fish.classList.remove("hide");
+
+  const close = fish.querySelector(".x-button");
+  close.onclick = () => {
+    fish.classList.add("hide");
+  };
+};
+
+//pop ups for the flies
+document.getElementById("dryfly").onclick = () => {
+  const fly = document.getElementById("modal-flyD");
+  fly.classList.remove("hide");
+
+  const close = fly.querySelector(".x-button");
+  close.onclick = () => {
+    fly.classList.add("hide");
+  };
+};
+document.getElementById("emergerfly").onclick = () => {
+  const fly = document.getElementById("modal-flyE");
+  fly.classList.remove("hide");
+
+  const close = fly.querySelector(".x-button");
+  close.onclick = () => {
+    fly.classList.add("hide");
+  };
+};
+document.getElementById("nymphfly").onclick = () => {
+  const fly = document.getElementById("modal-flyN");
+  fly.classList.remove("hide");
+
+  const close = fly.querySelector(".x-button");
+  close.onclick = () => {
+    fly.classList.add("hide");
+  };
+};
+document.getElementById("wetfly").onclick = () => {
+  const fly = document.getElementById("modal-flyW");
+  fly.classList.remove("hide");
+
+  const close = fly.querySelector(".x-button");
+  close.onclick = () => {
+    fly.classList.add("hide");
+  };
+};
