1b3cf4eb73c2c55af0c98b9c5844503ea5cef773
nkinard <nkinard7503@gmail.com>
2025-03-05 15:28:40 -0500

    json

--- csce242/projects/part6/spotlight-script.js
+++ csce242/projects/part6/spotlight-script.js
@@ -6,36 +6,6 @@ document.getElementById("hamburger-toggle").onclick = () => {
   document.getElementById("nav-list").classList.toggle("hide-small");
 };
 
-document.getElementById("spotlightS").onclick = () => {
-  const spot = document.getElementById("modal-spotlightS");
-  spot.classList.remove("hide");
-
-  const close = spot.querySelector(".x-button");
-  close.onclick = () => {
-    spot.classList.add("hide");
-  };
-};
-
-document.getElementById("spotlightW").onclick = () => {
-  const spot = document.getElementById("modal-spotlightW");
-  spot.classList.remove("hide");
-
-  const close = spot.querySelector(".x-button");
-  close.onclick = () => {
-    spot.classList.add("hide");
-  };
-};
-
-document.getElementById("spotlightM").onclick = () => {
-  const spot = document.getElementById("modal-spotlightM");
-  spot.classList.remove("hide");
-
-  const close = spot.querySelector(".x-button");
-  close.onclick = () => {
-    spot.classList.add("hide");
-  };
-};
-
 //https://nkinard.github.io/csce242/projects/part6/json/spotlight.json
 
 const getSpotlight = async () => {
@@ -51,8 +21,56 @@ const getSpotlight = async () => {
 const showSpotlight = async()=> {
   const spotlightDesign = await getSpotlight();
   const modalSpotlight = document.getElementById("modalSpotlight");
+  const pagediv = document.getElementById("spotlightlib");
+  const section = document.createElement("section");
+  section.classList = "f-container four-img-stack";
+  pagediv.append(section);
 
   spotlightDesign.forEach(async(spotlight)=>{
+    console.log(spotlight);
+    
+    //this part will create the main clickable buttons based on the json file
+    const divbox = document.createElement("div");
+    divbox.id = "sbox"+spotlight.name;
+    divbox.classList = "page-button-block";
+    section.append(divbox);
+
+    const a = document.createElement("a");
+    divbox.append(a);
+
+    const outerimage = document.createElement("img");
+    outerimage.src = spotlight.outerimage;
+    a.append(outerimage);
+    
+    const outername = document.createElement("h3");
+    outername.innerHTML = spotlight.name;
+    a.append(outername);
+
+
+    
+
+
+    /*
+    const outerh2 = document.createElement("h2");
+    outerh2.innerHTML = `${spotlight.name} River`;
+    pagediv.append(outerh2);
+
+    const spotlightitem = document.createElement("div");
+    spotlightitem.id = `${spotlight.name}`;
+    spotlightitem.classList = "page-button-block";
+    spotlightsection.append(spotlightitem);
+
+    const spotlightinfo = document.createElement("a");
+    spotlightsection.append(spotlightinfo);
+
+    const spotlightoutername = document.createElement("h3");
+    const spotlightouterimage = document.createElement("img");
+    spotlightouterimage.src = spotlight.outerimage;
+    spotlightoutername.innerHTML = spotlight.name;
+    spotlightsection.append(spotlightouterimage);
+    spotlightsection.append(spotlightoutername);
+    */
+
     //this part fully creates all the modals for them!
     const modaldiv = document.createElement("div");
     modaldiv.id = "modal-spotlight"+spotlight.name;
@@ -72,7 +90,25 @@ const showSpotlight = async()=> {
     modaldivtwo.classList = "modal-box-content";
     modaldiv.append(modaldivtwo);
 
+    //create the header image and name
+    const image = document.createElement("img");
+    image.src = spotlight.image;
+    modaldivtwo.append(image);
+    const name = document.createElement("h2");
+    name.innerHTML = spotlight.name;
+    modaldivtwo.append(name);
+
   });
 };
 
 showSpotlight();
+/*
+document.getElementById("spotlightC").onclick = () => {
+  const spot = document.getElementById("modal-spotlightChattooga");
+  spot.classList.remove("hide");
+
+  const close = spot.querySelector(".x-button");
+  close.onclick = () => {
+    spot.classList.add("hide");
+  }; 
+} */
